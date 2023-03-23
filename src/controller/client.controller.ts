b011c2c738de2e339d9e2c9ff125060877a59526
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-23 17:53:46 -0300

    update and delete client created

--- src/controller/client.controller.ts
+++ src/controller/client.controller.ts
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Client = mongoose.model("Client")
-import { Request, Response } from "express";
+import { request, Request, Response } from "express";
 
 exports.getClients = async (request: Request, response: Response) => {
   try {
@@ -25,6 +25,26 @@ exports.getClientById = async (request: Request, response: Response) => {
   try {
     const client = await Client.findOne({ _id: request.params.id })
     response.status(200).send(client)
+  } catch (error) {
+    response.status(400).send(error)
+  }
+}
+
+exports.updateClient = async (request: Request, response: Response) => {
+  try {
+    await Client.findByIdAndUpdate(request.params.id, {
+      $set: request.body
+    })
+    response.status(200).json({ message: "Cliente atualizado com sucesso!" })
+  } catch (error) {
+    response.status(400).send(error)
+  }
+}
+
+exports.deleteClient = async (request: Request, response: Response) => {
+  try {
+    await Client.findByIdAndDelete(request.params.id)
+    response.status(200).json({ message: "Cliente removido com sucesso!" })
   } catch (error) {
     response.status(400).send(error)
   }
--- src/routes/client.ts
+++ src/routes/client.ts
@@ -1,6 +1,8 @@
-const { getClients, createClient, getClientById } = require("../controller/client.controller")
+const { getClients, createClient, getClientById, updateClient, deleteClient } = require("../controller/client.controller")
 
 type ClientRoutes = {
+  delete: any
+  put: any
   post(arg0: string, createClient: any): unknown
   get: (arg0: string, arg1: any) => void 
 }
@@ -9,5 +11,7 @@ exports.clientRoutes = (app: ClientRoutes) => {
   app.get("/client", getClients)
   app.post("/client", createClient)
   app.get("/client/:id", getClientById)
+  app.put("/client/:id", updateClient)
+  app.delete("/client/:id", deleteClient)
 }
 
